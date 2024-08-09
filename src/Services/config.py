313291fe519322ba12f0f190a35598e4360cac1c
Solomon Oduyebo <124704556+SolomonGM@users.noreply.github.com>
2024-08-09 19:52:37 +0100

    Create config.py

---
+++ src/Services/config.py
@@ -0,0 +1,14 @@
+import os
+from pydantic import BaseSettings
+from typing import List
+
+class Settings(BaseSettings):
+    coingecko_api_url: str = "https://www.coingecko.com/en/all-cryptocurrencies"
+    supported_coins: List[str] = ["bitcoin", 'ethereum', 'litcoin', 'usdt', '']
+    supported_fiats: List[str] = ['usd']
+    update_intervals: int = 5 # Update the coin fetch every n minutes
+    
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
